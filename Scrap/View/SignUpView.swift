5b4d63d91ced86d2f5f21674272b04b1b2029320
donnyrla10 <kokoo1214@naver.com>
2022-09-19 18:02:09 +0900

    edit SignUpView UI

--- Scrap/View/SignUpView.swift
+++ Scrap/View/SignUpView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 import Combine
 
+let error_red = Color(red: 255/255, green: 84/255, blue: 84/255)
+let light_gray = Color(red: 217/255, green: 217/255, blue: 217/255)
+let bold_blue = Color(red: 20/255, green: 142/255, blue: 174/255)
+let light_blue = Color(red: 70/255, green: 193/255, blue: 241/255)
+
 struct SignUpView: View {
     @State private var username = ""
     @State private var id = ""
     @State private var pw = ""
     @State private var checkPW = ""
-    let maxUserName = Int(30)
+    let maxUserName = 30
     @Binding var rootView : Bool
-    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode> //pop
 
     var backButton : some View { //custom back button
         Button(action: {
@@ -27,6 +32,7 @@ struct SignUpView: View {
                     .foregroundColor(.black)
                 Text("회원가입") //translated Back button title
                     .foregroundColor(.black)
+                    .fontWeight(.semibold)
             }
         }
     }
@@ -34,75 +40,121 @@ struct SignUpView: View {
     var body: some View {
         VStack(spacing: 32){
             VStack{
-                Text("이름")
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .leading)
-                    .font(.system(size: 20, weight: .semibold))
-                TextField("이름을 입력하세요", text: $username)
-                    .padding(.horizontal, 20)
-                    .onReceive(Just(username), perform: { _ in
-                        if maxUserName < username.count {
-                            username = String(username.prefix(maxUserName))
+                VStack{
+                    HStack{
+                        Text("이름")
+                            .font(.system(size: 20, weight: .semibold))
+                        Text("*")
+                            .foregroundColor(bold_blue)
+                            .padding(.leading, -2)
+                    }
+                    .frame(width: UIScreen.main.bounds.width/1.2, alignment: .leading)
+                    TextField("이름을 입력하세요", text: $username)
+                        .frame(width: UIScreen.main.bounds.width/1.2, height: 28, alignment: .leading)
+                        .onReceive(Just(username), perform: { _ in
+                            if maxUserName < username.count {
+                                username = String(username.prefix(maxUserName))
+                            }
+                        })
+                    Divider()
+                        .foregroundColor(light_gray)
+                        .frame(width: UIScreen.main.bounds.width/1.2)
+                    Text("*이름을 입력하세요.") //관련 에러 메세지 따로 출력되도록
+                        .font(.caption)
+                        .foregroundColor(error_red)
+                        .frame(width: UIScreen.main.bounds.width/1.2, alignment: .leading)
+                        .hidden()
+                }
+                .padding(.bottom, 16)
+                VStack{
+                    HStack{
+                        Text("아이디")
+                            .font(.system(size: 20, weight: .semibold))
+                        Text("*")
+                            .foregroundColor(bold_blue)
+                            .padding(.leading, -2)
+                    }
+                    .frame(width: UIScreen.main.bounds.width/1.2, alignment: .leading)
+                    VStack{
+                        HStack{
+                            TextField("아이디를 입력하세요", text: $id)
+                                .frame(width: UIScreen.main.bounds.width/1.65, height: 28, alignment: .leading)
+                            Button(action: {
+                                //아이디 중복 확인 버튼
+                            }){
+                                Text("중복 확인")
+                                    .padding()
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .frame(width: 80, height: 32, alignment: .center)
+                                    .foregroundColor(Color.white)
+                                    .background(light_blue)
+                                    .cornerRadius(8)
+                            }
                         }
-                    })
-                Divider()
-                    .frame(width: UIScreen.main.bounds.width - 40)
-                Text("*이름을 입력하세요.") //관련 에러 메세지 따로 출력되도록
-                    .font(.caption)
-                    .foregroundColor(Color.red)
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .topLeading)
-//                    .visibility(.gone)
+                        Divider()
+                            .foregroundColor(light_gray)
+                            .frame(width: UIScreen.main.bounds.width/1.2)
+                    }
+                    Text("*아이디를 입력하세요.") //관련 에러 메세지 따로 출력되도록
+                        .font(.caption)
+                        .foregroundColor(error_red)
+                        .frame(width: UIScreen.main.bounds.width/1.2, alignment: .leading)
+                        .hidden()
+                }
+                .padding(.bottom, 16)
+                VStack{
+                    HStack{
+                        Text("비밀번호")
+                            .font(.system(size: 20, weight: .semibold))
+                        Text("*")
+                            .foregroundColor(bold_blue)
+                            .padding(.leading, -2)
+                    }
+                    .frame(width: UIScreen.main.bounds.width/1.2, alignment: .leading)
+                    TextField("비밀번호를 입력하세요", text: $pw)
+                        .frame(width: UIScreen.main.bounds.width/1.2, height: 28, alignment: .leading)
+                    Divider()
+                        .foregroundColor(light_gray)
+                        .frame(width: UIScreen.main.bounds.width/1.2)
+                    Text("*비밀번호를 입력하세요.") //관련 에러 메세지 따로 출력되도록
+                        .font(.caption)
+                        .foregroundColor(error_red)
+                        .frame(width: UIScreen.main.bounds.width/1.2, alignment: .topLeading)
+                        .hidden()
+                }
+                .padding(.bottom, 16)
+                VStack{
+                    HStack{
+                        Text("비밀번호 확인")
+                            .font(.system(size: 20, weight: .semibold))
+                        Text("*")
+                            .foregroundColor(bold_blue)
+                            .padding(.leading, -2)
+                    }
+                    .frame(width: UIScreen.main.bounds.width/1.2, alignment: .leading)
+                    TextField("비밀번호 확인을 입력하세요", text: $checkPW)
+                        .frame(width: UIScreen.main.bounds.width/1.2, height: 28, alignment: .leading)
+                    Divider()
+                        .frame(width: UIScreen.main.bounds.width/1.2)
+                    Text("*비밀번호 확인을 입력하세요.") //관련 에러 메세지 따로 출력되도록
+                        .font(.caption)
+                        .foregroundColor(error_red)
+                        .frame(width: UIScreen.main.bounds.width/1.2, alignment: .topLeading)
+                        .hidden()
+                }
+                .padding(.bottom, 16)
             }
-            VStack{
-                Text("아이디")
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .leading)
-                    .font(.system(size: 20, weight: .semibold))
-                TextField("아이디를 입력하세요", text: $id)
-                    .padding(.horizontal, 20)
-                Divider()
-                    .frame(width: UIScreen.main.bounds.width - 40)
-                Text("*아이디를 입력하세요.") //관련 에러 메세지 따로 출력되도록
-                    .font(.caption)
-                    .foregroundColor(Color.red)
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .topLeading)
-//                    .visibility(.gone)
-            }
-            VStack{
-                Text("비밀번호")
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .leading)
-                    .font(.system(size: 20, weight: .semibold))
-                TextField("비밀번호를 입력하세요", text: $pw)
-                    .padding(.horizontal, 20)
-                Divider()
-                    .frame(width: UIScreen.main.bounds.width - 40)
-                Text("*비밀번호를 입력하세요.") //관련 에러 메세지 따로 출력되도록
-                    .font(.caption)
-                    .foregroundColor(Color.red)
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .topLeading)
-//                    .visibility(.gone)
-            }
-            VStack{
-                Text("비밀번호 확인")
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .leading)
-                    .font(.system(size: 20, weight: .semibold))
-                TextField("비밀번호 확인을 입력하세요", text: $checkPW)
-                    .padding(.horizontal, 20)
-                Divider()
-                    .frame(width: UIScreen.main.bounds.width - 40)
-                Text("*비밀번호 확인을 입력하세요.") //관련 에러 메세지 따로 출력되도록
-                    .font(.caption)
-                    .foregroundColor(Color.red)
-                    .frame(width: UIScreen.main.bounds.width - 40, alignment: .topLeading)
-//                    .visibility(.gone)
-            }
+            
             //모두 올바른 입력값인 경우 회원가입 성공 -> HomeView 이동
             //실패하면 버튼 클릭 x -> 비활성화하면 어떨지
             //우선 상관없이 바로 HomeView 이동 가능하도록
-            NavigationLink(destination: HomeView(rootView: $rootView).navigationBarBackButtonHidden(true).navigationBarHidden(true)){
+            NavigationLink(destination: MainHomeView().navigationBarBackButtonHidden(true).navigationBarHidden(true)){
                 Text("회원가입")
-                    .frame(width: UIScreen.main.bounds.width / 2, height: 40, alignment: .center)
+                    .font(.system(size: 16, weight: .semibold))
+                    .frame(width: UIScreen.main.bounds.width / 2.2, height: 44, alignment: .center)
                     .foregroundColor(Color.white)
-                    .background(Color.blue)
-                    .cornerRadius(12)
+                    .background(light_blue)
+                    .cornerRadius(10)
             }
             .padding(.top, 28)
         }
@@ -115,25 +167,6 @@ struct SignUpView: View {
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-//        SignUpView()
-        LoginView()
+        SignUpView(rootView: .constant(true))
     }
-}
-
-enum Visibility: CaseIterable {
-  case visible, // view is fully visible
-       invisible, // view is hidden but takes up space
-       gone // view is fully removed from the view hierarchy
-}
-
-extension View {
-  @ViewBuilder func visibility(_ visibility: Visibility) -> some View {
-    if visibility != .gone {
-      if visibility == .visible {
-        self
-      } else {
-        hidden()
-      }
-    }
-  }
 }
